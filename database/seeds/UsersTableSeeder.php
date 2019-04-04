19a19ef7c98c08f3184dc881d79e7b726d63ef9a
vagrant <vagrant@homestead>
2019-04-04 10:08:21 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'count';
         $user->email = 'count@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
